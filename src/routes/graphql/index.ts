4e814287546afcae8406a9b834398a27ab6a077c
ilonaand <ilonaand@gmail.com>
2023-01-29 19:23:28 +0300

    feat: query

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,5 +1,15 @@
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
 import { graphqlBodySchema } from './schema';
+import { user, post, memberType, profile } from './types';
+
+import {
+  graphql,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+} from 'graphql';
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
@@ -11,7 +21,62 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         body: graphqlBodySchema,
       },
     },
-    async function (request, reply) {}
+    async function (request, reply) {
+      const RootQuery = new GraphQLObjectType({
+        name: 'query',
+        fields: {
+          users: {
+            type: new GraphQLList(user),
+            resolve () {
+              return  fastify.db.users.findMany();
+            },
+          },
+          profiles: {
+            type: new GraphQLList(profile),
+            resolve() {
+              return fastify.db.profiles.findMany();
+            },
+          },
+          posts: {
+            type: new GraphQLList(post),
+            resolve() {
+              return fastify.db.posts.findMany();
+            },
+          },
+          memberTypes: {
+            type: new GraphQLList(memberType),
+            resolve() {
+              return fastify.db.memberTypes.findMany();
+            },
+          },
+        }}
+      ); 
+
+      const roorMutation =  new GraphQLObjectType({
+        name: 'Mutation',
+        fields: {
+          createUser: {
+            type: user,
+            args: {
+              firstName: { type: new GraphQLNonNull(GraphQLString) },
+              lastName: { type: new GraphQLNonNull(GraphQLString) },
+              email: { type: new GraphQLNonNull(GraphQLString) },
+            },
+            async resolve(_, args) {
+              const user = await fastify.db.users.create({
+                firstName: args.firstName,
+                lastName: args.lastName,
+                email: args.email,
+              });
+
+              return user;
+            },
+          },
+        },
+      });
+      const schema = new GraphQLSchema({ query: RootQuery, mutation: roorMutation });
+      return await graphql({ schema, source: request.body.query! });
+    }
   );
 };
 
